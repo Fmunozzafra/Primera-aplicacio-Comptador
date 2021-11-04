4c0ea9238fabce11453d335ac4d0daf448942778
Fmunozzafra <fmunoz@iesebre.com>
2021-11-04 16:06:07 +0100

    Molts de errors a causa de carpetes noves, he tornat a crear el repositori de Github

---
+++ app/src/main/java/me/ferranmunozzafra/dam/comptador/MainActivity.kt
@@ -0,0 +1,101 @@
+package me.ferranmunozzafra.dam.comptador
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private val INITIAL_TIME = 20
+
+    private val TAG = MainActivity::class.java.simpleName
+
+    internal lateinit var tapMeButton : Button
+    internal lateinit var timeTextView : TextView
+    internal lateinit var counterTextView : TextView
+    internal var counter=0
+    internal var time= INITIAL_TIME
+
+    internal var appStarted = false
+    internal lateinit var countdownTimer : CountDownTimer
+    //internal val initialCountDownTimer : Long = 60000
+    internal val initialCountDownTimer : Long = time.toLong()*1000
+    internal val intervalCountDownTimer : Long = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,"Hola mon! onCreate")
+        Log.d(TAG,counter.toString())
+        Log.d(TAG,time.toString())
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initCountdown()
+
+        tapMeButton = findViewById(R.id.tapMeButton)
+        timeTextView = findViewById(R.id.timeTextView)
+        counterTextView = findViewById(R.id.counterTextView)
+
+
+
+        tapMeButton.setOnClickListener(){
+            if(!appStarted){
+                startGame()
+            }
+
+            incrementCounter()
+        }
+
+        timeTextView.text = getString(R.string.timeText, time)
+    }
+
+    private fun startGame() {
+        countdownTimer.start()
+        appStarted = true
+    }
+
+    private fun initCountdown(){
+        countdownTimer = object : CountDownTimer(initialCountDownTimer,intervalCountDownTimer){
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                timeTextView.text = timeLeft.toString()
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+
+        }
+
+    }
+
+    private fun incrementCounter(){
+        counter += 1
+        counterTextView.text = counter.toString()
+
+    }
+
+    private fun endGame(){
+        Toast.makeText(this, getString(R.string.endGame, counter), Toast.LENGTH_LONG).show()
+        resetGame()
+    }
+
+    private fun resetGame(){
+        //Reset del comptador a 0
+        counter = 0
+        counterTextView.text=counter.toString()
+        //Reinicialitzar el comptador
+        time=INITIAL_TIME
+        timeTextView.text = time.toString()
+        initCountdown()
+
+        //Tornar a posar game started a false per a poder tornar a iniciar la aplicació des de 0
+        appStarted = false
+
+    }
+}
