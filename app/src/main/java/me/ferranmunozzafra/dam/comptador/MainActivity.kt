0bf0ce8db5b896e5cf71a20f5f067c0f665e7949
Fmunozzafra <fmunoz@iesebre.com>
2021-11-04 16:53:00 +0100

    Afegit Dialog Alert amb la info de l'aplicació

--- app/src/main/java/me/ferranmunozzafra/dam/comptador/MainActivity.kt
+++ app/src/main/java/me/ferranmunozzafra/dam/comptador/MainActivity.kt
@@ -34,10 +34,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -84,6 +87,28 @@ class MainActivity : AppCompatActivity() {
     } else {
       resetGame()
     }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    super.onCreateOptionsMenu(menu)
+    menuInflater.inflate(R.menu.menu,menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    if (item.itemId == R.id.actionAbout) {
+      showInfo()
+    }
+    return true
+  }
+
+  private fun showInfo() {
+    val dialogTitle = getString(R.string.aboutTitle, BuildConfig.VERSION_NAME)
+    val dialogMessage = getString(R.string.aboutMessage)
+
+    val builder = AlertDialog.Builder(this)
+
+    builder.setTitle(dialogTitle).setMessage(dialogMessage).create().show()
   }
 
   override fun onSaveInstanceState(outState: Bundle) {
@@ -172,3 +197,6 @@ class MainActivity : AppCompatActivity() {
     resetGame()
   }
 }
+
+
+
